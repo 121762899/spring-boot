16546f4e64382a0897fcabea2583b663e5e11f22
zhangxuesong <zhangxuesong01@meicai.cn>
2019-05-16 11:17:44 +0800

    添加log4j2 日志

--- src/main/java/com/spring/boot/controller/UserController.java
+++ src/main/java/com/spring/boot/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.spring.boot.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,14 +10,21 @@ import com.github.pagehelper.PageInfo;
 import com.spring.boot.entity.User;
 import com.spring.boot.service.UserService;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
+@Slf4j
 public class UserController {
 
     @Autowired
     private UserService userService;
 
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+    
     @RequestMapping("/findUserList")
     public PageInfo<User> findUserList(int page, int size) {
+        log.info("This is lombok log");
+        logger.info("this is apache log");
         return userService.findUserList(page, size);
     }
     
