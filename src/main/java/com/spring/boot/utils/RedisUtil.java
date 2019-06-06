31790877de5dfd9a033f3573d3557261c61ed482
zhangxuesong <zhangxuesong01@meicai.cn>
2019-06-06 13:55:58 +0800

    添加另一种分布式锁

--- src/main/java/com/spring/boot/utils/RedisUtil.java
+++ src/main/java/com/spring/boot/utils/RedisUtil.java
@@ -1,10 +1,13 @@
 package com.spring.boot.utils;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.Transaction;
 
 @Component
 public class RedisUtil {
@@ -22,6 +25,33 @@ public class RedisUtil {
                 return false;
             }
         }
+    }
+    
+    
+    private boolean lockTask(String redisLockKey) {
+        boolean isOK = false;
+        try (Jedis jedis = jedisPool.getResource()) {
+            int retryTime = 3;
+            while(--retryTime >= 0 && !isOK) {
+                Transaction tx = jedis.multi();
+                tx.get(redisLockKey);
+                tx.setex(redisLockKey, 10, "1");
+                List<Object> redisReturn = tx.exec();
+                if(redisReturn.get(0) != null) {
+                    isOK = false;
+                    Thread.sleep(1000);
+                } else {
+                    isOK = true;
+                    break;
+                }
+            }
+        } catch(Throwable e) {
+            isOK = true;
+        }
+//        if(isOK == false) {
+//            throw new BusinessException(1240, "任务被锁定,请退出重试");
+//        }
+        return isOK;
     }
     
 }
